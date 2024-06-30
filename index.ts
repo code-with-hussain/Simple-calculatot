45fb91a13738f09f1c5080d87c4408b8252f19d7
Muhammad  Hussain <zainjahangir4567@gmail.com>
2024-06-30 04:28:12 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,84 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// printing a Wellcome Message
+console.log("\n\tWellcome To \'CodeWithzain\' - CLI Simple calculator\n");
+
+
+// Asking Question form Users through inquirer
+let answers = await inquirer.prompt([
+    {message: "Enter First Number ", type: "number", name: "firstNumber"},
+    {message: "Enter Second Number", type: "number", name: "secondNumber"},
+    {
+        message: "Select One Operator to perform operations",
+        type: "list",
+        name:"operator",
+        choices: ["Addition", "Subtractions", "Multipication", "Division"],
+    },
+]);
+console.log(answers)
+
+// // considtional statment if else 
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstName + answers.secondNumber)
+}
+
+else if (answers.operator === "Subtractions"){
+    console.log(answers.firstName - answers.secondNumber)
+}
+
+else if (answers.operator === "Multipications"){
+    console.log(answers.firstName * answers.secondNumber)
+}
+
+else if (answers.operator === "Division"){
+    console.log(answers.firstName / answers.secondNumber)
+}
+
+else{
+    console.log("Invaild Input")
+}
+
+
+
+
+// ===============================================================================
+// { messeage:"enter 1st number",type: "number",name : "firstNumber"},
+// { messeage:"enter 2nd number",type: "number",name : "secondNumber"},
+// {
+//  message: "select one operator 4 operation",
+//  type: "list",
+//  name: "operator",
+//  choices:["addition", "substraction","multiplication","division"],
+
+// },
+
+
+// ]);
+// console.log(answers);
+
+// //if conditional statment
+
+// if(answers.operator === "addition")
+// {
+// console.log(answers.firstNumber + answers.secondNumber)
+// }
+// else if(answers.operator === "substraction")
+//        {
+//   console.log(answers.firstNumber - answers.secondNumber)
+//               }
+       
+// else if(answers.operator === "multiplication")
+//  {
+//  console.log(answers.firstNumber * answers.secondNumber)
+//  }
+//  else if(answers.operator === "division")
+//  {
+//  console.log(answers.firstNumber / answers.secondNumber)
+//  }
+//  else
+//  {
+//        console.log("invalid input")
+//  }
